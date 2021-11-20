75c3cb2fe3f08ba9745bbeb2bb64a00a7e75d8ab
kimpoiprince <kimpoiprince@gmail.com>
2021-11-20 09:40:13 +0800

    product
    遊戲清單

---
+++ routers/product/product.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+// 密碼加密套件
+const bcrypt = require("bcrypt");
+// 資料庫連線要求定義
+const connection = require("../../connection/db.js");
+
+// 撈出遊戲資料
+router.get("/", async (req, res) => {
+  let data = await connection.queryAsync("SELECT * FROM product");
+  res.json(data);
+});
+
+module.exports = router;
