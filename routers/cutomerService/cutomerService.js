bf34b8ffab581a77f9faca32bd8aa3b2a889b8b9
yuki-crystal <bloodpinna@gmail.com>
2021-11-21 14:32:21 +0800

    remove put

--- routers/cutomerService/cutomerService.js
+++ routers/cutomerService/cutomerService.js
@@ -15,7 +15,6 @@ router.get("/questions", async (req, res) => {
 
 
 router.post("/questions", async (req, res) => {
-  console.log("req.body", req.body);
   let data = await connection.queryAsync(
     "INSERT INTO question (user_id, category, subcategory, content, created_at) VALUES (?)",
     [
@@ -29,30 +28,6 @@ router.post("/questions", async (req, res) => {
     ]
   );
   res.send(req.body);
-});
-
-router.put("/memSelf/:account", async (req, res) => {
-  //   let userInsert = await connection.queryAsync(
-  //     "INSERT INTO member (address, birth, gender, name, phone, photo) VALUE (?,?,?,?,?,?) WHERE account = ?;",
-  //     [req.params.account]
-  //   );
-  let userUpdate = await connection.queryAsync(
-    "UPDATE member SET address = ?, birth = ?, gender =? , email = ?, name=?,phone=? WHERE account = ?",
-    [
-      req.body.address,
-      req.body.birth,
-      req.body.gender,
-      req.body.email,
-      req.body.name,
-      req.body.phone,
-      req.params.account,
-    ]
-  );
-
-  if (req.session.member) {
-    console.log(userUpdate);
-    res.json({ message: "更新成功" });
-  }
 });
 
 module.exports = router;
