c71d9c6ec37d0ad3bf401409760193b6ee6ace4e
Ya-Wen,Chan <89444541+YawenChan@users.noreply.github.com>
2021-12-04 21:36:02 +0800

    更新登出登入後問題

--- routers/loginRegister/auth.js
+++ routers/loginRegister/auth.js
@@ -155,7 +155,7 @@ router.post("/login", async (req, res) => {
 
 //只要登出，所以從網址get就可以
 router.get("/logout", (req, res) => {
-  req.session.member = null;
+  req.session.destroy();
   // 讓cookie立刻過期(為了登出後可以讓忘記密碼正常使用)
   res.cookie("userSession", "", { expires: new Date() });
   res.json({ message: "登出成功" });
